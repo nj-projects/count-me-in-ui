f884b980295e377802ab8882d4cc95c12cdb4e8b
Nolan-James-Dev <nj@nolanjames.me>
2024-07-17 09:17:54 +0100

    update navigation bar depending on page

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,29 +1,23 @@
 import {Component, inject, signal} from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import {RouterLink} from "@angular/router";
-import {Location} from "@angular/common";
+import {Location, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     MatIcon,
-    RouterLink
+    RouterLink,
+    NgIf
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   location = inject(Location);
-  currentUrl = signal('');
-
 
   constructor() {
-  }
-
-  updateUrl() {
-    console.log(this.location.path())
-    this.currentUrl.set(this.location.path());
   }
 
 }
