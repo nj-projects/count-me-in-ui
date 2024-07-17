8847372fe4223865cf595ad85740a0366574d7b2
james-nolan-dev <james@jamesnolan.me>
2024-07-17 22:51:14 +0100

    create event

--- src/app/components/event/create/create.component.ts
+++ src/app/components/event/create/create.component.ts
@@ -1,10 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, effect, inject} from '@angular/core';
 import {MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
-import {MatInput} from "@angular/material/input";
-import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
+import {MatInput, MatInputModule} from "@angular/material/input";
+import {
+  MatDatepicker,
+  MatDatepickerInput,
+  MatDatepickerModule,
+  MatDatepickerToggle
+} from "@angular/material/datepicker";
 import {MatNativeDateModule} from "@angular/material/core";
 import {MatButton} from "@angular/material/button";
-import {RouterLink} from "@angular/router";
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {EventService} from "../event.service";
+import moment from "moment/moment";
+import {EventRequest} from "../model/event.model";
 
 @Component({
   selector: 'app-create',
@@ -12,18 +21,59 @@ import {RouterLink} from "@angular/router";
   imports: [
     MatFormField,
     MatInput,
+    MatInputModule,
     MatDatepickerInput,
     MatDatepickerToggle,
     MatDatepicker,
+    MatDatepickerModule,
     MatHint,
     MatLabel,
     MatNativeDateModule,
     MatButton,
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
+  providers: [MatDatepickerModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
 
+  eventService = inject(EventService);
+  route = inject(ActivatedRoute);
+  router = inject(Router);
+  fb = inject(FormBuilder);
+
+  event = {} as EventRequest;
+
+  form = this.fb.group({
+    name: ['', Validators.required],
+    description: ['', Validators.required],
+    date: ['', Validators.required],
+  })
+
+  constructor() {
+    this.listenCreateEvent();
+  }
+
+
+  createEvent() {
+    this.event.name = this.form.value.name!;
+    this.event.description = this.form.value.description!;
+    this.event.date = moment(this.form.value.date!).format('DD-MM-yyyy').toString();
+
+    console.log(this.event.date);
+    console.log(this.event.name);
+
+    this.eventService.create(this.event);
+  }
+
+  private listenCreateEvent() {
+    effect(() => {
+      const newEvent = this.eventService.createSignal();
+      if (newEvent.status === 'OK' && newEvent.value) {
+        this.router.navigate(['/management'])
+      }
+    })
+  }
 }
