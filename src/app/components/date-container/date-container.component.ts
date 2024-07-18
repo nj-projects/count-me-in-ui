81615f2cbbf11396718c0297756e1875b73d96ab
Nolan-James-Dev <nj@nolanjames.me>
2024-07-18 12:55:08 +0100

    Event Carousel for multiple events

--- src/app/components/date-container/date-container.component.ts
+++ src/app/components/date-container/date-container.component.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component, input, OnInit} from '@angular/core';
 import {CustomDate} from "./model/date.model";
 import {MatGridList, MatGridTile} from "@angular/material/grid-list";
 
@@ -12,6 +12,25 @@ import {MatGridList, MatGridTile} from "@angular/material/grid-list";
   templateUrl: './date-container.component.html',
   styleUrl: './date-container.component.scss'
 })
-export class DateContainerComponent {
-  customDate = input<Partial<CustomDate>>({});
+export class DateContainerComponent implements OnInit {
+
+  // customDate = input<Partial<CustomDate>>({});
+  customDate = {} as CustomDate;
+  eventDate = input<Partial<string>>('');
+
+  ngOnInit(): void {
+    this.setCountdown();
+  }
+
+  private setCountdown() {
+    setInterval(() => {
+      const now = new Date().getTime();
+      let toDate = Date.parse(this.eventDate()) - now;
+
+      this.customDate.days = Math.floor(toDate / (1000 * 60 * 60 * 24));
+      this.customDate.hours = Math.floor((toDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.customDate.mins = Math.floor((toDate % (1000 * 60 * 60)) / (1000 * 60));
+      this.customDate.secs = Math.floor((toDate % (1000 * 60)) / (1000));
+    })
+  }
 }
--- src/app/components/event/event.component.ts
+++ src/app/components/event/event.component.ts
@@ -2,12 +2,11 @@ import {Component, effect, inject, OnInit} from '@angular/core';
 import {EventService} from "./event.service";
 import {EventResponse} from "./model/event.model";
 import {DatePipe} from "@angular/common";
-import {count} from "rxjs";
 import {DateCardComponent} from "../date-container/date-card/date-card.component";
 import {DateContainerComponent} from "../date-container/date-container.component";
-import {CustomDate} from "../date-container/model/date.model";
 import {MatButton} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
+import {NgbCarouselModule} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-event',
@@ -17,7 +16,8 @@ import {RouterLink} from "@angular/router";
     DateCardComponent,
     DateContainerComponent,
     MatButton,
-    RouterLink
+    RouterLink,
+    NgbCarouselModule
   ],
   templateUrl: './event.component.html',
   styleUrl: './event.component.scss'
@@ -27,8 +27,8 @@ export class EventComponent implements OnInit {
   events: EventResponse[] = [];
   event: EventResponse | undefined = undefined;
   loading: boolean = false;
-  countdown: string = "";
-  customDate = {} as CustomDate;
+  // todo - add image url to api
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
   constructor() {
     this.listenGetAllEvents();
@@ -49,24 +49,7 @@ export class EventComponent implements OnInit {
       if (allEvents.status == 'OK' && allEvents.value) {
         this.loading = false;
         this.events = allEvents.value;
-        this.event = allEvents.value[0];
-        this.setCountdown();
       }
     });
-  }
-
-  protected readonly count = count;
-
-  private setCountdown() {
-    if (this.events.length > 0) {
-      setInterval(() => {
-        const now = new Date().getTime();
-        const toDate = Date.parse(this.event!.date) - now;
-        this.customDate.days = Math.floor(toDate / (1000 * 60 * 60 * 24));
-        this.customDate.hours = Math.floor((toDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        this.customDate.mins = Math.floor((toDate % (1000 * 60 * 60)) / (1000 * 60));
-        this.customDate.secs = Math.floor((toDate % (1000 * 60)) / (1000));
-      })
-    }
   }
 }
